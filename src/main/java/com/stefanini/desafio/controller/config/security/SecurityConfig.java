7df6d3ffb5f40868d0b0b8a2ffa25f06cfea8f58
Ewerton Costa <jecs.ewerton@gmail.com>
2020-02-20 19:33:37 -0300

    Adicionando dependencia jjwt para que a API funcione no conceito stateless. Adicionado controller para gerenciar autenticação. WIP

--- src/main/java/com/stefanini/desafio/controller/config/security/SecurityConfig.java
+++ src/main/java/com/stefanini/desafio/controller/config/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
@@ -25,8 +26,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers( HttpMethod.GET, "/pessoas").permitAll()
-        .anyRequest().authenticated().and().formLogin();
+        .antMatchers( HttpMethod.GET, "/pessoas").permitAll()
+        .antMatchers( HttpMethod.POST, "/auth").permitAll()
+        .anyRequest().authenticated()
+        .and().csrf().disable()
+        .sessionManagement().sessionCreationPolicy( SessionCreationPolicy.STATELESS );
     }
 
     @Override
